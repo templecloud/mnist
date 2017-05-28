77bd5d3defa8f4f9529d97c9e88f6a6a2a6f2a56
TempleCloud <tim.langford@gmail.com>
2017-05-28 23:49:29 +0100

    Added functions for reading MNIST IDX files.

--- mnist.go
+++ mnist.go
@@ -1,16 +1,260 @@
 package main
 
-//--------------------------------------------------------------------------------------------------
-// http://yann.lecun.com/exdb/mnist/
+import (
+	"os"
+)
 
 import (
 	"compress/gzip"
 	"io"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 )
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"image"
+	"image/png"
+)
+
+//==================================================================================================
+// ByteImage
+
+// An in-memory image stored as a consecuative array of bytes.
+type ByteImage struct {
+	width  int
+	height int
+	bytes  []byte
+}
+
+//  Get the image bytes as a formatted matrix.
+func (img *ByteImage) pixels() [][]byte {
+	byteIdx := 0
+	pixels := make([][]byte, img.height)
+	for i := 0; i < img.height; i++ {
+		pixels[i] = make([]byte, img.width)
+		for j := 0; j < img.width; j++ {
+			pixels[i][j] = img.bytes[byteIdx]
+			byteIdx++
+		}
+	}
+	return pixels
+}
+
+//func (img *ByteImage) normalise() []float64 {
+//
+//	normalised := make([]float64, len(img.bytes))
+//	for i := 0; i < len(img.bytes); i++ {
+//		img.bytes[i] = float64(img.bytes[i] / 256)
+//	}
+//
+//	return normalised
+//}
+
+//==================================================================================================
+// MNIST IDX file format
+
+//--------------------------------------------------------------------------------------------------
+// IDX Data Type
+
+// The data type of an IdxMagic number.
+type IdxDataType struct {
+	code     int
+	name     string
+	numBytes int
+}
+
+// Create an IdxDataType from the specified byte.
+func makeIdxDataType(dataType byte) (IdxDataType, error) {
+	var idxDataType IdxDataType
+	var err error
+	switch {
+	case dataType == 0x08:
+		idxDataType = IdxDataType{0x08, "unsigned", 1}
+	case dataType == 0x09:
+		idxDataType = IdxDataType{0x09, "signed", 1}
+	case dataType == 0x0B:
+		idxDataType = IdxDataType{0x0B, "short", 2}
+	case dataType == 0x0C:
+		idxDataType = IdxDataType{0x0C, "int", 4}
+	case dataType == 0x0D:
+		idxDataType = IdxDataType{0x0D, "float", 4}
+	case dataType == 0x0E:
+		idxDataType = IdxDataType{0x0E, "double", 4}
+	case true:
+		err = fmt.Errorf("Unknown IdxDataType for byte: %x", dataType)
+	}
+	return idxDataType, err
+}
+
+//--------------------------------------------------------------------------------------------------
+// IDX Magic Number
+
+// The magic number of an IdxHeader.
+type IdxMagic struct {
+	nilBytes      bool
+	dataType      IdxDataType
+	numDimensions int
+}
+
+// Create an IdxMagic from the specified magic bytes.
+func makeIdxMagic(magicBytes []byte) IdxMagic {
+	nilBytes, _ := makeNilBytes(magicBytes[0:2])
+	idxDataType, _ := makeIdxDataType(magicBytes[2:3][0])
+	numDimensions, _ := makeNumDimensions(magicBytes[3:])
+	return IdxMagic{nilBytes, idxDataType, numDimensions}
+}
+
+// Return true if the specified magic bytes have the required number of nil bytes.
+func makeNilBytes(nilBytes []byte) (bool, error) {
+	var result bool
+	var err error
+	value, _ := binary.Uvarint(nilBytes)
+	if len(nilBytes) == 2 && value == 0 {
+		result = true
+	} else {
+		err = fmt.Errorf("Bad nilByte in header: %x", nilBytes)
+	}
+	return result, err
+}
+
+// Return the number of dimensions specified in the dimension byte of the magic number header.
+func makeNumDimensions(numDimByte []byte) (int, error) {
+	// return int(numDimByte[0]), nil
+	var numDimensions uint8
+	err := binary.Read(bytes.NewReader(numDimByte), binary.BigEndian, &numDimensions)
+	return int(numDimensions), err
+}
+
+//--------------------------------------------------------------------------------------------------
+// IDX Header
+
+// The header structure of an IDX format file
+type IdxHeader struct {
+	idxMagic   IdxMagic
+	dimensions []int
+}
+
+func makeDimension(dimensionBytes []byte) (int, error) {
+	var dimension uint32
+	err := binary.Read(bytes.NewReader(dimensionBytes), binary.BigEndian, &dimension)
+	return int(dimension), err
+}
+
+func (idx *IdxHeader) fileHeaderOffset() int {
+	return 4 + (idx.idxMagic.numDimensions * 4)
+}
+
+// Read the IDXHeader from an IDX data format file
+func ReadIdxHeader(idxFile *os.File) (IdxHeader, []error) {
+	// collect all errors
+	errors := make([]error, 0)
+	// reset to file start
+	idxFile.Seek(0, 0)
+	// read magic bytes
+	magicBytes, err := readBytes(idxFile, 4)
+	idxMagic := makeIdxMagic(magicBytes)
+	errors = appendError(errors, err)
+	// read dimensional meta data
+	dimensions := make([]int, idxMagic.numDimensions)
+	for n := 0; n < idxMagic.numDimensions; n++ {
+		dimNBytes, err := readBytes(idxFile, 4)
+		errors = appendError(errors, err)
+		dimensions[n], err = makeDimension(dimNBytes)
+		errors = appendError(errors, err)
+	}
+	// return the header
+	return IdxHeader{idxMagic, dimensions}, errors
+}
+
+func readBytes(file *os.File, numBytes int) ([]byte, error) {
+	bytes := make([]byte, numBytes)
+	_, err := file.Read(bytes)
+	return bytes, err
+}
+
+func appendError(errors []error, error error) []error {
+	return append(errors, error)
+}
+
+//--------------------------------------------------------------------------------------------------
+// IDX ByeImage store functions
+
+// Read a ByteImage from an IDX format data file.
+func ReadIdxImage(idxFile *os.File, imageIdx int) (ByteImage, error) {
+	// extract header
+	idxHeader, _ := ReadIdxHeader(idxFile)
+	// compute images values
+	fileHeaderOffset := idxHeader.fileHeaderOffset()
+	dataWidth := idxHeader.idxMagic.dataType.numBytes
+	imgWidth := idxHeader.dimensions[1]
+	imgHeight := idxHeader.dimensions[2]
+	imgSize := imgWidth * imgHeight * dataWidth
+	// calculate offsets (as int64 to obey first seek result)
+	currentOffset, _ := idxFile.Seek(0, 1)
+	startOffset := int64(fileHeaderOffset + (imgSize * imageIdx))
+	endOffset := startOffset + int64(imgSize)
+	// extract images bytes
+	idxFile.Seek(startOffset-currentOffset, 1)
+	imageBytes := make([]byte, endOffset-startOffset)
+	_, err := idxFile.Read(imageBytes)
+	// build result
+	img := ByteImage{imgWidth, imgHeight, imageBytes}
+	// return the image
+	return img, err
+}
+
+// Read a set of ByteImages from an IDX format data file.
+func ReadIdxImages(idxFile *os.File, imageIdxs []int) ([]ByteImage, error) {
+	numImages := len(imageIdxs)
+	images := make([]ByteImage, numImages)
+	for i := 0; i < numImages; i++ {
+		images[i], _ = ReadIdxImage(idxFile, i)
+	}
+	return images, nil
+}
+
+// Read all ByteImages from an IDX format data file.
+func ReadAllIdxImages(idxFile *os.File) ([]ByteImage, error) {
+	// extract header and numImages
+	idxHeader, _ := ReadIdxHeader(idxFile)
+	numImages := idxHeader.dimensions[0]
+	// extract images
+	images := make([]ByteImage, numImages)
+	for i := 0; i < numImages; i++ {
+		images[i], _ = ReadIdxImage(idxFile, i)
+	}
+	return images, nil
+}
+
+// Generate a PNG format image file from the speicified ByteImage.
+func WritePngImage(byteImage ByteImage, pngFile *os.File) (string, error) {
+	// compute images values
+	imgWidth := byteImage.width
+	imgHeight := byteImage.height
+	// create image
+	imgRect := image.Rect(0, 0, imgWidth, imgHeight)
+	img := image.NewGray(imgRect)
+	img.Pix = byteImage.bytes
+	img.Stride = imgWidth
+	// write image
+	err := png.Encode(pngFile, img)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return pngFile.Name(), err
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+//==================================================================================================
+// MNIST IDX ByteImageStore downloader
+
+//--------------------------------------------------------------------------------------------------
+// http://yann.lecun.com/exdb/mnist/
 
 const (
 	MNIST_LOCAL        = "./data/"
@@ -22,7 +266,6 @@ const (
 )
 
 func gunzip(source string, destination string) (err error) {
-
 	// open an input stream on the source path
 	in, err := os.Open(source)
 	if err != nil {
@@ -75,24 +318,19 @@ func download(url string, filepath string) (err error) {
 }
 
 func downloadMNIST() (err error) {
-
 	// ensure the specified local directory exist
 	err = os.MkdirAll(MNIST_LOCAL, os.FileMode(0777))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	// download and unarchive each mnist file
 	var mnistFiles = [...]string{
 		MNIST_TRAIN_IMAGES, MNIST_TRAIN_LABELS, MNIST_TEST_IMAGES, MNIST_TEST_LABELS}
-
 	for _, v := range mnistFiles {
-
 		// download zipped file
 		source := MNIST_REMOTE + v
 		destination := MNIST_LOCAL + v
-		unarchived := strings.TrimSuffix(destination, ".gz")
-
+		unarchived := strings.TrimSuffix(destination, ".gz") + ".idx"
 		if _, err := os.Stat(destination); os.IsNotExist(err) {
 			toDownload := Download{source, destination}
 			log.Printf("Downloading File: %+v\n", toDownload)
@@ -103,7 +341,6 @@ func downloadMNIST() (err error) {
 		} else {
 			log.Printf("Already downloaded: %s\n", destination)
 		}
-
 		// unarchive
 		if _, err := os.Stat(unarchived); os.IsNotExist(err) {
 			log.Printf("Local File     : %s\n", destination)
@@ -128,4 +365,5 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+
 }
---
+++ mnist_test.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+)
+
+//--------------------------------------------------------------------------------------------------
+
+// Set-up and executes tests.
+func TestMain(t *testing.M) {
+	/// call flag.Parse() here if TestMain uses flags
+	// Clean 'test results' directory
+	os.RemoveAll(TestOutput)
+	os.MkdirAll(TestOutput, os.FileMode(0777))
+
+	os.Exit(t.Run())
+}
+
+// Assertions test function.
+func assertEqual(t *testing.T, a interface{}, b interface{}, message string) {
+	if a == b {
+		return
+	} else {
+		message = fmt.Sprintf("%v != %v\n=> ", a, b) + message
+		t.Fatal(message)
+	}
+}
+
+//--------------------------------------------------------------------------------------------------
+
+func TestConvertType(t *testing.T) {
+	var res IdxDataType
+	var err error
+
+	res, err = makeIdxDataType(byte(0x08))
+	if res.name != "unsigned" && err == nil {
+		t.Error("Expected 'unsigned'")
+	}
+
+	res, err = makeIdxDataType(byte(0x09))
+	if res.name != "signed" && err != nil {
+		t.Error("Expected 'signed'")
+	}
+
+	res, err = makeIdxDataType(byte(0x0B))
+	if res.name != "short" && err != nil {
+		t.Error("Expected 'short'")
+	}
+
+	res, err = makeIdxDataType(byte(0x0C))
+	if res.name != "int" && err != nil {
+		t.Error("Expected 'int'")
+	}
+
+	res, err = makeIdxDataType(byte(0x0D))
+	if res.name != "float" && err != nil {
+		t.Error("Expected 'float'")
+	}
+
+	res, err = makeIdxDataType(byte(0x0E))
+	if res.name != "double" && err != nil {
+		t.Error("Expected 'double'")
+	}
+
+	res, err = makeIdxDataType(byte(0x01))
+	if err.Error() != "Unknown IdxDataType for byte: 1" {
+		t.Error("Expected an error for byte 0x01")
+	}
+}
+
+//--------------------------------------------------------------------------------------------------
+// MNIST IDX file base tests
+
+const (
+	MNISTPath   = "./data/"
+	IDXFileName = "t10k-images-idx3-ubyte"
+	IDXFileExt  = ".idx"
+	IDXFile     = IDXFileName + IDXFileExt
+	MNIST       = MNISTPath + IDXFile
+	TestOutput  = "./test-results/"
+	ImageFile   = TestOutput + IDXFileName
+)
+
+func TestExtractIdxHeader(t *testing.T) {
+	idxFile, _ := os.Open(MNIST)
+	output, _ := ReadIdxHeader(idxFile)
+	fmt.Printf("output: %v\n", output)
+}
+
+func TestReadReadIdxImage(t *testing.T) {
+	idxFile, _ := os.Open(MNIST)
+	ReadIdxImage(idxFile, 5000)
+	fmt.Printf("image created\n")
+}
+
+func TestReadReadIdxImages(t *testing.T) {
+	idxFile, _ := os.Open(MNIST)
+	imageIdxs := []int{0, 999, 1999, 2999, 3999, 4999, 5999, 6999, 7999, 8999, 9999}
+	images, _ := ReadIdxImages(idxFile, imageIdxs)
+	fmt.Printf("num images created: %v\n", len(images))
+	assertEqual(t, len(imageIdxs), len(images), "The correct number of images are extracted.")
+}
+
+func TestReadAllIdxImages(t *testing.T) {
+	idxFile, _ := os.Open(MNIST)
+	header, _ := ReadIdxHeader(idxFile)
+	images, _ := ReadAllIdxImages(idxFile)
+	fmt.Printf("num images created: %v\n", len(images))
+	assertEqual(t, header.dimensions[0], len(images), "All images are extracted.")
+}
+
+func TestReadIdxWritePngImage(t *testing.T) {
+
+	idxFile, _ := os.Open(MNIST)
+	header, _ := ReadIdxHeader(idxFile)
+
+	imageIdx := 0
+	outputBytes, _ := ReadIdxImage(idxFile, imageIdx)
+	pngFile, _ := os.Create(ImageFile + "-" + strconv.Itoa(imageIdx) + ".png")
+	outputImage, _ := WritePngImage(outputBytes, pngFile)
+	fmt.Printf("outputImage: %v\n", outputImage)
+
+	imageIdx1 := 6666
+	outputBytes1, _ := ReadIdxImage(idxFile, imageIdx1)
+	pngFile1, _ := os.Create(ImageFile + "-" + strconv.Itoa(imageIdx1) + ".png")
+	outputImage1, _ := WritePngImage(outputBytes1, pngFile1)
+	fmt.Printf("outputImage: %v\n", outputImage1)
+
+	imageIdx2 := header.dimensions[0] - 1
+	outputBytes2, _ := ReadIdxImage(idxFile, imageIdx2)
+	pngFile2, _ := os.Create(ImageFile + "-" + strconv.Itoa(imageIdx2) + ".png")
+	outputImage2, _ := WritePngImage(outputBytes2, pngFile2)
+	fmt.Printf("outputImage: %v\n", outputImage2)
+}
